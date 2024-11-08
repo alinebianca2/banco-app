f4b5af1ac579ab906f20e30f3c07e6c3e0044a6f
“Aline <alinebianca204@gmail.com>
2024-11-08 13:52:14 -0300

    feat: appComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClienteComponent } from './modules/cliente/cliente/cliente.component';
+import { TransacoesComponent } from './modules/transacoes/transacoes/transacoes.component';
+import { ExtratoComponent } from './modules/extrato/extrato/extrato.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: '/cliente', pathMatch: 'full' },  
+  { path: 'cliente', component: ClienteComponent },
+  { path: 'transacoes/:clientId', component: TransacoesComponent },
+  { path: 'extrato/:clientId', component: ExtratoComponent }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
