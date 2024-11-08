99cefa5b6391545201fc3dfc4d5a5314aaabe238
“Aline <alinebianca204@gmail.com>
2024-11-08 13:52:40 -0300

    feat: client module

--- src/app/modules/cliente/cliente.module.ts
+++ src/app/modules/cliente/cliente.module.ts
@@ -3,8 +3,15 @@ import { CommonModule } from '@angular/common';
 import { ClienteComponent } from './cliente/cliente.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { ReactiveFormsModule } from '@angular/forms'; 
-
-
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 
 @NgModule({
@@ -14,8 +21,16 @@ import { ReactiveFormsModule } from '@angular/forms';
   imports: [
     CommonModule,
     SharedModule,
-    ReactiveFormsModule
-
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatSelectModule,
+    MatToolbarModule,
+    MatSnackBarModule
   ]
 })
 export class ClienteModule { }
--- src/app/modules/cliente/cliente/cliente.component.ts
+++ src/app/modules/cliente/cliente/cliente.component.ts
@@ -1,22 +1,30 @@
-import { Component } from '@angular/core';
-import { Cliente } from '../client.model';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { ClienteService } from '../cliente.service';
 
 @Component({
   selector: 'app-cliente',
   templateUrl: './cliente.component.html',
   styleUrls: ['./cliente.component.scss']
 })
-export class ClienteComponent {
+export class ClienteComponent implements OnInit {
 
-  clienteForm: FormGroup;
+  clienteForm!: FormGroup;
+  clienteCadastrado = false;
+  clienteId: number = 0;  
+  clientes: Array<any> = [];  
 
-  constructor(private fb: FormBuilder) {
-    this.clienteForm = this.fb.group({
-      nome: ['', [Validators.required, Validators.minLength(3)]],
-      idade: ['', [Validators.required, Validators.min(18)]],
-      email: ['', [Validators.required, Validators.email]],
-      numeroConta: ['', [Validators.required, Validators.pattern(/^\d+$/)]]
+  constructor(private snackBar: MatSnackBar, private router: Router, private clienteService: ClienteService) {
+  }
+
+  ngOnInit(): void {
+    this.clienteForm = new FormGroup({
+      nome: new FormControl('', [Validators.required, Validators.minLength(3)]), 
+      idade: new FormControl('', [Validators.required, Validators.min(18), Validators.max(120)]), 
+      email: new FormControl('', [Validators.required, Validators.email]),
+      numeroConta: new FormControl('', [Validators.required, Validators.pattern('^[0-9]{10,12}$')])
     });
   }
 
@@ -24,13 +32,45 @@ export class ClienteComponent {
     return this.clienteForm.valid;
   }
 
+
   onSubmit() {
     if (this.clienteForm.valid) {
-      console.log('Cliente cadastrado:', this.clienteForm.value);
-      this.clienteForm.reset(); 
+      const clienteData = this.clienteForm.value;
+
+      this.clienteService.cadastrarCliente(clienteData).subscribe(
+        (response) => {
+          this.clienteId = response.id; 
+          this.clienteCadastrado = true;
+
+          this.snackBar.open(`Cliente cadastrado com sucesso! ID: ${this.clienteId}`, 'Fechar', {
+            duration: 3000,
+            panelClass: ['success-snackbar'],
+          });
+
+          this.clienteForm.reset();
+        },
+        (error) => {
+          console.error('Erro ao cadastrar cliente', error);
+          this.snackBar.open('Erro ao cadastrar cliente. Tente novamente.', 'Fechar', {
+            duration: 3000,
+            panelClass: ['error-snackbar'],
+          });
+        }
+      );
     } else {
       console.log('Por favor, preencha todos os campos corretamente.');
     }
+  }
+
+
+  irParaTransacoes() {
+    this.clienteCadastrado = false;
+    this.router.navigate(['/transacoes', this.clienteId]);
+
+  }
+
+  get f() {
+    return this.clienteForm.controls;
   }
 }
 
